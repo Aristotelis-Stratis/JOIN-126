916a5ab4325bb1c1628f999f399feaac57b9a3fb
Eduard Fray <eduard.fray@hotmail.de>
2024-04-03 21:20:14 +0200

    Board to JS transfer start

--- js/board.js
+++ js/board.js
@@ -8,9 +8,89 @@ async function init(){
     console.log(allTasks);
 }
 
-function popUpCheckBlue() {
-    let check = document.getElementById('subtask-check');
-    check.src = "./assets/img/icons/checkbox-checked-black-24.png";
+async function loadTasksFromStorage() {
+    try {
+        const tasksString = await getItem('tasks');
+        if (tasksString) {
+            const tasks = JSON.parse(tasksString);
+            allTasks = tasks;     // Update the global tasks array
+        } else {
+            console.log('No tasks found. Starting with an empty task list.');
+        }
+    } catch (e) {
+        console.warn('Could not load tasks:', e);
+        allTasks = [];               // Reset the tasks array on failure
+    }
+
+    showToDos();
+}
+
+
+function showToDos(){
+   let todo = document.getElementById('ToDos');
+   todo.innerHTML = '';
+   
+
+    for (let i = 0; i < allTasks.length; i++) {
+        const task = allTasks[i];
+        let taskName = task.title;
+        let taskDescription = task.description;
+        let usersHTML = '';
+
+        for (let j = 0; j < task.contacts.length; j++) {
+            const user = task.contacts[j];
+            
+            let userInitials = user.initials;
+            let userColor = user.color;
+
+            usersHTML += `
+            <span class="contact-icon board-icon" style="background-color: ${userColor};">${userInitials}</span>
+            `;
+        }
+
+        todo.innerHTML += `
+        <div>
+            <div class="cardA" onclick="showPopUp()">
+                  <span class="task-category-board">User Story</span>
+                  <div class="card-middle-part">
+                    <h4 class="task-name">${taskName}</h4>
+                    <span class="task-description">${taskDescription}</span>
+                  </div>
+                  <div class="subtasks">
+                    <div class="subtask-bar">
+                      <div class="filled-subtask-bar"></div>
+                    </div><span>1/2 Subtasks</span>
+                  </div>
+                  <div class="asigned-to">
+                    <div class="asigned-to-icons">
+                        ${usersHTML}
+                    </div>
+                    <div class="asigned-to-image-container">
+                      <img src="./assets/img/icons/medium.png" alt="medium-png">
+                    </div>
+                    </div>
+            </div>
+        </div>
+        `;
+    }
+}
+
+function popUpCheckDark() {
+    let check = document.getElementById('subtask-check1');
+    if (check.src.includes('checkbox-checked-black-24.png')) {
+        check.src = "./assets/img/icons/checkbox-empty-black-24.png";
+    } else {
+        check.src = "./assets/img/icons/checkbox-checked-black-24.png";
+    }
+}
+
+function popUpCheckDark2() {
+    let check = document.getElementById('subtask-check2');
+    if (check.src.includes('checkbox-checked-black-24.png')) {
+        check.src = "./assets/img/icons/checkbox-empty-black-24.png";
+    } else {
+        check.src = "./assets/img/icons/checkbox-checked-black-24.png";
+    }
 }
 
 function showPopUp() {
@@ -38,19 +118,4 @@ function closePopUp() {
 
 function doNotClosePopUp(event) {
     event.stopPropagation();
-}
-
-async function loadTasksFromStorage() {
-    try {
-        const tasksString = await getItem('tasks');
-        if (tasksString) {
-            const tasks = JSON.parse(tasksString);
-            allTasks = tasks;     // Update the global tasks array
-        } else {
-            console.log('No tasks found. Starting with an empty task list.');
-        }
-    } catch (e) {
-        console.warn('Could not load tasks:', e);
-        allTasks = [];               // Reset the tasks array on failure
-    }
 }
