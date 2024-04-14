e41888dc6b51c5b6584fe8aec322d7a692364b05
Aristotelis-Stratis <stratis.dev@outlook.com>
2024-04-14 15:46:48 +0200

    pathing active class

--- script.js
+++ script.js
@@ -33,55 +33,23 @@ async function includeHTML() {
             element.innerHTML = 'Page not found';
         }
     }
+    activeMenu();
 }
 
 
 /**
- * Determines if a click event's target is within a specified element.
- * 
- * @param {string} elementId - The ID of the target element.
- * @param {EventTarget} target - The click event's target.
- * @returns {boolean} True if target is inside the element, false otherwise.
+ * Updates the 'active' class on menu items based on the current path.
  */
-function isClickInside(elementId, target) {
-    const element = document.getElementById(elementId);
-    return element && element.contains(target);
-}
+function activeMenu() {
+    const menuItems = document.querySelectorAll('.menu-choice a');
+    const currentPath = window.location.pathname;
 
-
-/**
- * Toggles the visibility of a dropdown menu, hiding it if visible, and resets the arrow icon's rotation.
- * 
- * @param {string} menuId - The ID of the dropdown menu.
- * @param {string} arrowIconId - The ID of the associated arrow icon.
- */
-function closeDropdownMenu(menuId, arrowIconId) {
-    const dropdownMenu = document.getElementById(menuId);
-    const arrowIcon = document.getElementById(arrowIconId);
-    const isVisible = dropdownMenu.classList.contains('visible') || dropdownMenu.style.display === 'flex';
-    if (isVisible) {
-        if (dropdownMenu.classList.contains('visible')) {
-            dropdownMenu.classList.remove('visible');
+    menuItems.forEach(item => {
+        const itemPath = item.getAttribute('href').replace(/^\.\//, '');
+        if (currentPath.endsWith(itemPath)) {
+            item.closest('.menu-choice').classList.add('active');
         } else {
-            dropdownMenu.style.display = 'none';
+            item.closest('.menu-choice').classList.remove('active');
         }
-        if (arrowIcon) {
-            arrowIcon.style.transform = '';
-        }
-    }
+    });
 }
-
-
-/**
- * Handles document-wide click events to close dropdown menus if clicked outside.
- * This listener checks clicks against the 'assignedTo' and 'category' dropdowns.
- * It closes a dropdown if the click occurred outside its area or its associated input.
- */
-document.addEventListener('click', function (event) {
-    if (!isClickInside('assignedTo', event.target) && !isClickInside('assign-dropdown-menu', event.target)) {
-        closeDropdownMenu('assign-dropdown-menu', 'arrow-assign-to');
-    }
-    if (!isClickInside('selected-option', event.target) && !isClickInside('category-dropdown-menu', event.target)) {
-        closeDropdownMenu('category-dropdown-menu', 'arrow-category');
-    }
-});
