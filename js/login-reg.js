3df647a8d82414ed911a2ed047e69660854bfa5b
Aristotelis-Stratis <stratis.dev@outlook.com>
2024-05-09 18:10:03 +0200

    call / firebase update2

--- js/login-reg.js
+++ js/login-reg.js
@@ -101,43 +101,60 @@ async function initRegistry() {
 async function login() {
     let email = document.getElementById('email').value;
     let password = document.getElementById('password').value;
-    const inputs = document.querySelectorAll('input');
 
-    let user = await getUserByEmail(email);
-    if (user && user.password === password) {
-        console.log('Login erfolgreich!');
-        rememberCheck();
-        setCurrentUser(user);
-        window.location.href = 'summary.html';
+    // Entferne Sonderzeichen aus der E-Mail-Adresse
+    let cleanedEmail = email.replace(/[^\w\s]/gi, '');
+
+    let usersData = await loadData(`users/${cleanedEmail}`);
+    console.log("Geladene Benutzerdaten:", usersData);
+
+    if (usersData) {
+        // Zugriff auf das erste Element im usersData Objekt
+        let userKey = Object.keys(usersData)[0];
+        let user = usersData[userKey];
+
+        if (user && user.password === password) {
+            console.log('Login erfolgreich!');
+            await setCurrentUser(user, userKey, cleanedEmail); // cleanedEmail als Argument übergeben
+
+            // Verzögere die Weiterleitung um 5000 Millisekunden (5 Sekunden)
+            setTimeout(() => {
+                window.location.href = 'contacts.html';
+            }, 5000);
+        } else {
+            inputValidation('email', 'emailErrorField', ' ');
+            inputValidation('password', 'passwordErrorField', 'Invalid email or password.');
+            console.log('Login fehlgeschlagen. Bitte überprüfe deine Anmeldedaten und versuche es erneut.');
+        }
     } else {
         inputValidation('email', 'emailErrorField', ' ');
-        inputValidation('password', 'passwordErrorField', 'Invalid email or password.');
-        console.log('Login fehlgeschlagen. Bitte überprüfe deine Anmeldedaten und versuche es erneut.');
+        console.log('Benutzer nicht gefunden. Bitte überprüfe deine Anmeldedaten und versuche es erneut.');
     }
 }
 
-// Funktion für den Gästelogin
-function loginAsGuest() {
-    setCurrentUser(allUsers[0]);
-    console.log('Logged in as guest:', currentUser);
-    window.location.href = 'contacts.html';
-}
 
 
-async function setCurrentUser(user) {
-    currentUser = user;
-    await setItem('currentUser', JSON.stringify(currentUser));
-    console.log('Current user set successfully:', currentUser);
-}
-
-
-async function getCurrentUser() {
+async function loginAsGuest() {
     try {
-        const userString = await getItem('currentUser');
-        if (userString) {
-            const user = JSON.parse(userString);
-            console.log('Current user retrieved:', user);
-            return user;
+        // Konvertiere die E-Mail des Gastbenutzers in eine cleanedEmail
+        let guestEmail = "guest@example.com";
+        let cleanedEmail = guestEmail.replace(/[^\w\s]/gi, ''); // Entfernt alle Sonderzeichen
+        let guestUserId = "guest";
+
+        // Verwende den neuen Pfad, der cleanedEmail und userId einschließt
+        let guestUser = await loadData(`users/${cleanedEmail}/${guestUserId}`);
+        if (guestUser) {
+            console.log('Logged in as guest:', guestUser);
+
+            // Setze die Standardwerte für den Gastbenutzer
+            localStorage.setItem('currentUserId', guestUserId);
+            localStorage.setItem('cleanedEmail', cleanedEmail);
+
+            // Setze den Gastbenutzer als aktuellen Benutzer
+            await setCurrentUser(guestUser, guestUserId, cleanedEmail);
+            setTimeout(() => {
+                window.location.href = 'contacts.html';
+            }, 5000); // Kurze Verzögerung für die Demonstration
         } else {
             console.log('Guest user not found. Please check the database setup.');
         }
@@ -168,7 +185,7 @@ async function ensureGuestUserExists() {
             ],
             board: {
                 name: "Initial Board",
-                todo: [{ title: "Task 1", status: "pending" }]
+                items: [{ title: "Task 1", status: "pending" }]
             },
             summary: {
                 totalTasks: 1,
@@ -277,6 +294,25 @@ function checkCheckbox(checkbox) {
     }
 }
 
+function animationValidation() {
+    if (document.getElementById('overlay')) {
+        removeOverlay();
+    } else {
+        return false;
+    }
+}
+
+function removeOverlay() {
+    let overlay = document.getElementById('overlay');
+    let logo = document.getElementById('main-logo');
+
+    setTimeout(() => {
+        overlay.classList.add('d-none');
+        logo.classList.remove('d-none');
+    }, 2000);
+}
+
+
 function loadRememberData() {
     try {
     let rememberEmail = JSON.parse(localStorage.getItem('email'));
@@ -314,24 +350,166 @@ function saveUserData() {
     localStorage.setItem('password', `"${getValue('password')}"`);
 }
 
-function animationValidation() {
-    if (document.getElementById('overlay')) {
-        removeOverlay();
-    } else {
-        return false;
-    }
-}
-
-function removeOverlay() {
-    let overlay = document.getElementById('overlay');
-    let logo = document.getElementById('main-logo');
-
-    setTimeout(() => {
-        overlay.classList.add('d-none');
-        logo.classList.remove('d-none');
-    }, 2000);
-}
-
 function startSlideInUpAnim() {
     getById('reg-overlay').classList.remove('d-none');
 }
+
+
+
+// OLD
+
+
+// async function getCurrentUser() {
+//     try {
+//         const userString = await getItem('currentUser');
+//         if (userString) {
+//             const user = JSON.parse(userString);
+//             console.log('Current user retrieved:', user);
+//             return user;
+//         } else {
+//             console.log('No current user found.');
+//             return null;
+//         }
+//     } catch (error) {
+//         console.error('Failed to get current user:', error);
+//         return null;
+//     }
+// }
+
+
+// async function saveToStorage() {
+//     await setItem('allUsers', JSON.stringify(allUsers));
+// }
+
+
+// async function loadAllUserFromStorage() {
+//     try {
+//         const response = await getItem('allUsers');
+//         if (response && response.length > 0) {
+//             allUsers = JSON.parse(response);
+//         } else {
+//             console.log('No users found in storage, using default array.');
+//             allUsers = [guestUser];  // Reset to default with guest user
+//         }
+//     } catch (error) {
+//         console.error('Failed to load users from storage:', error);
+//         allUsers = [guestUser]; // Fallback to default on error
+//     }
+// }
+
+
+// function deleteStorage() {
+//     allUsers = [];
+//     setItem('allUsers', JSON.stringify(allUsers));
+// }
+
+// async function setCurrentUser(user) {
+//     currentUser = user;
+//     await setItem('currentUser', JSON.stringify(currentUser));
+//     console.log('Current user set successfully:', currentUser);
+// }
+
+// Funktion für den Gästelogin
+// function loginAsGuest() {
+//     setCurrentUser(allUsers[0]);
+//     console.log('Logged in as guest:', currentUser);
+//     window.location.href = 'contacts.html';
+// }
+
+
+// function createUser(username, email, password) {
+//     return {
+//         id: generateUniqueId(),
+//         name: username,
+//         email: email,
+//         password: password,  // Das Passwort sollte sicher gespeichert werden (Hash)
+//         data: {
+//             contacts: [],
+//             tasks: [],
+//             board: {},
+//             summary: {}
+//         }
+//     };
+// }
+
+
+// function generateUniqueId() {
+//     return Date.now().toString(36) + Math.random().toString(36).substr(2);
+// }
+
+
+// async function getUserByEmail(email) {
+//     await loadAllUserFromStorage();
+//     return allUsers.find(user => user.email === email);
+// }
+
+
+// async function saveUserToStorage(user) {
+//     try {
+//         await loadAllUserFromStorage();
+//         allUsers.push(user);
+//         await setItem('allUsers', JSON.stringify(allUsers));
+//     } catch (error) {
+//         console.error('Failed to save users:', error);
+//     }
+// }
+
+
+// Logik für reguläres Login
+// async function login() {
+//     let email = document.getElementById('email').value;
+//     let password = document.getElementById('password').value;
+//     const inputs = document.querySelectorAll('input');
+
+//     let user = await getUserByEmail(email);
+//     if (user && user.password === password) {
+//         console.log('Login erfolgreich!');
+//         setCurrentUser(user);
+//         window.location.href = 'summary.html';
+//     } else {
+//         inputValidation('email', 'emailErrorField', ' ');
+//         inputValidation('password', 'passwordErrorField', 'Invalid email or password.');
+//         console.log('Login fehlgeschlagen. Bitte überprüfe deine Anmeldedaten und versuche es erneut.');
+//     }
+// }
+
+
+
+// async function initRegistry() {
+//     let username = document.getElementById('name').value;
+//     let email = document.getElementById('email').value;
+//     let password = document.getElementById('password').value;
+//     let userExists = await getUserByEmail(email);
+
+//     if (!userExists) {
+//         let newUser = createUser(username, email, password);
+//         // Füge den Benutzer zu seinen eigenen Kontakten hinzu
+//         let newContact = createContactObject(username, email, ''); // Hier kein Telefonnummer vorgegeben
+//         newUser.data.contacts.push(newContact);
+//         await saveUserToStorage(newUser);
+//         console.log('Du hast dich erfolgreich registriert!');
+//         window.setTimeout(() => { window.location.href = "login.html"; }, 2500);
+//     } else {
+//         console.log('Die Emailadresse existiert bereits!');
+//     }
+// }
+
+
+// Initialisierungsfunktion, die den Gastbenutzer sicherstellt
+// async function initializeUsers() {
+//     // await loadAllUserFromStorage();
+//     if (allUsers.length === 0 || !allUsers.some(user => user.id === 'guest')) {
+//         allUsers.unshift(guestUser); // Fügt den Gastbenutzer am Anfang des Arrays hinzu
+//         // await setItem('allUsers', JSON.stringify(allUsers)); // Speichert das aktualisierte Array
+//     }
+// }
+
+
+// async function init() {
+//     //await initializeUsers(); // Stelle sicher, dass Gastbenutzer vorhanden ist
+//     console.log('allUsers: ', allUsers);
+//     const inputs = document.querySelectorAll('input');
+//     startEventlistener(inputs);
+//     animationValidation();
+//     eventListenerKeyup(inputs);
+// }
