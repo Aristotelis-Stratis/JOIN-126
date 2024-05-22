e25bf95d831575bdcc7e3279e95bdeaea790b00f
IvanLaumann <laumann.ivan@gmail.com>
2024-05-22 18:16:05 +0200

    countUrgentTasks()

--- js/summary.js
+++ js/summary.js
@@ -11,15 +11,20 @@ function updateSummary() {
     let done = getById('done').innerHTML = path.done.length;
     let inProgress = getById('inProgress').innerHTML = path.inProgress.length;
     let feedback =  getById('feedback').innerHTML = path.awaitFeedback.length;
-    let urgent = getById('urgent').innerHTML = path.todo.length; // <--- Pfad ändern
 
-    let sum = todo + done + urgent + inProgress + feedback;
+    let sum = todo + done + inProgress + feedback;
 
     getById('allTasks').innerHTML = sum;
+    countUrgentTasks(path);
 }
 
 
 function getById(id) {
     let element = document.getElementById(id);
     return element;
+}
+
+
+function countUrgentTasks(path) {
+    getById('urgent').innerHTML = Object.values(path).flat().filter(task => task.priority === 'urgent').length;
 }
